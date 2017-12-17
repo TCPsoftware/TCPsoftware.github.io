ed8176596ce05b581c98b97ae058eaaeeb05f2e7
涂CP <1691957831@qq.com>
2017-12-17 22:42:39 +0800

    Update https.js

--- cdn/js/https.js
+++ cdn/js/https.js
@@ -1,9 +1,9 @@
-if(location.protocol == "http:")
+        if(location.protocol == "http:")
 {
     var s = location.href;
     s = s.replace("http:","https:");
     window.location = s;
 }
 var cnzz_script = document.createElement("script");
-cnzz_script.src="./cnzz.js";
+cnzz_script.src="https://s19.cnzz.com/z_stat.php?id=1271202395&web_id=1271202395";
 document.getElementsByTagName("head")[0].appendChild(cnzz_script);
