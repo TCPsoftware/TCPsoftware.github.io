a4f04f3cfb20564387b6dd0b29eb8c273660ddf6
tcpsoft <1691957831@qq.com>
2020-02-09 16:26:55 +0800

    Site updated: 2020-02-09 16:26:52

--- js/diy.js
+++ js/diy.js
@@ -4,3 +4,14 @@
 link = document.getElementsByClassName("post-copyright-link")[0].children[1];
 href = link.getAttribute("href").replace("/http:/","http://");
 link.setAttribute("href",href);
+
+window.onload = function(){
+	img=document.getElementsByTagName("img")
+	for(i=0;i<img.length;i++ ){
+		//console.log(img[i] )
+		//console.log(i )
+		src=img[i].getAttribute("src")
+		src=src.replace("../../","https://blogsrc-1301043685.cos.ap-chengdu.myqcloud.com/")
+		img[i].setAttribute("src",src)
+	}
+}
